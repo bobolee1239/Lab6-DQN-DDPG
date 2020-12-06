ee6740c7d975223223b315f4221650f121a566a9
Brian Lee <bobolee1239@gmail.com>
2020-12-06 12:06:14 +0800

    dqn done

--- dqn-example.py
+++ dqn-example.py
@@ -1,18 +1,23 @@
 '''DLP DQN Lab'''
-__author__ = 'chengscott'
+__author__    = 'brian-th.lee'
 __copyright__ = 'Copyright 2020, NCTU CGI Lab'
-import argparse
-from collections import deque
-import itertools
-import random
-import time
-
+# -----------------------------------------------
 import gym
-import numpy as np
+import time
 import torch
-import torch.nn as nn
-from torch.utils.tensorboard import SummaryWriter
+import random
+import logging
+import argparse
+import itertools
 
+import numpy    as np
+import torch.nn as nn
+
+from torch.utils.tensorboard import SummaryWriter
+from collections import deque
+
+logging.basicConfig(level=logging.DEBUG)
+# -----------------------------------------------
 
 class ReplayMemory:
     __slots__ = ['buffer']
@@ -24,7 +29,7 @@ class ReplayMemory:
         return len(self.buffer)
 
     def append(self, *transition):
-        # (state, action, reward, next_state, done)
+        # (state, action, reward, next_state, not_done)
         self.buffer.append(tuple(map(tuple, transition)))
 
     def sample(self, batch_size, device):
@@ -36,42 +41,84 @@ class ReplayMemory:
 
 class Net(nn.Module):
     def __init__(self, state_dim=8, action_dim=4, hidden_dim=32):
-        super().__init__()
-        ## TODO ##
-        raise NotImplementedError
+        super(Net, self).__init__()
+
+        self.act = nn.ReLU()
+
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+
+        self.adv = nn.Linear(hidden_dim, action_dim)
+        self.var = nn.Linear(hidden_dim, 1)
 
     def forward(self, x):
-        ## TODO ##
-        raise NotImplementedError
+        '''
+            - x : tensor in shape of (N, state_dim)
+        '''
+        h1 = self.fc1(x)
+        h1 = self.act(h1)
+
+        h2 = self.fc2(h1)
+        h2 = self.act(h2)
+
+        advantage = self.adv(h2)
+        value     = self.var(h2)
+        avg_adv   = torch.mean(advantage, dim=-1, keepdim=True)
+        Q_value   = value + advantage - avg_adv
+
+        return Q_value
 
 
 class DQN:
     def __init__(self, args):
         self._behavior_net = Net().to(args.device)
-        self._target_net = Net().to(args.device)
+        self._target_net   = Net().to(args.device)
+        # -------------------------------------------
         # initialize target network
+        # -------------------------------------------
         self._target_net.load_state_dict(self._behavior_net.state_dict())
-        ## TODO ##
-        # self._optimizer = ?
-        raise NotImplementedError
+
+        self._optimizer = torch.optim.RMSprop(lr=args.lr)
+        self._criteria  = nn.MSELoss()
         # memory
-        self._memory = ReplayMemory(capacity=args.capacity)
+        self._memory    = ReplayMemory(capacity=args.capacity)
 
         ## config ##
-        self.device = args.device
-        self.batch_size = args.batch_size
-        self.gamma = args.gamma
-        self.freq = args.freq
+        self.device      = args.device
+        self.batch_size  = args.batch_size
+        self.gamma       = args.gamma
+        self.freq        = args.freq
         self.target_freq = args.target_freq
 
+    def select_best_action(self, state):
+        '''
+            - state: (state_dim, )
+        '''
+        state  = DQN.reshape_input_state(state)
+        qvars  = self._behavior_net(state)      # (1, act_dim)
+        action = torch.argmax(qvars, dim=-1)    # (1, )
+
+        return action.item()
+
     def select_action(self, state, epsilon, action_space):
-        '''epsilon-greedy based on behavior network'''
-         ## TODO ##
-        raise NotImplementedError
+        '''
+        epsilon-greedy based on behavior network
+
+            -state = (state_dim, )
+        '''
+        if random.random() < epsilon:
+            return np.random.randint(0, action_space, size=1)[0]
+        else:
+            return select_best_action(state)
 
     def append(self, state, action, reward, next_state, done):
-        self._memory.append(state, [action], [reward / 10], next_state,
-                            [int(done)])
+        self._memory.append(
+            state, 
+            [action], 
+            [reward / 10], 
+            next_state,
+            [int(not done)]
+        )
 
     def update(self, total_steps):
         if total_steps % self.freq == 0:
@@ -81,17 +128,19 @@ class DQN:
 
     def _update_behavior_network(self, gamma):
         # sample a minibatch of transitions
-        state, action, reward, next_state, done = self._memory.sample(
-            self.batch_size, self.device)
+        ret = self._memory.sample(self.batch_size, self.device)
+        state, action, reward, next_state, 2cont = ret 
 
-        ## TODO ##
-        # q_value = ?
-        # with torch.no_grad():
-        #    q_next = ?
-        #    q_target = ?
-        # criterion = ?
-        # loss = criterion(q_value, q_target)
-        raise NotImplementedError
+        q_values      = self._behavior_net(state)       # (N, act_dim)
+        q_value, act  = torch.max(q_values, dim=-1)     # (N, )
+
+        with torch.no_grad():
+           qs_next     = self._target_net(next_state)   # (N, act_dim)
+           q_next, act = torch.max(qs_next, dim=-1)
+           q_target = q_next + reward
+
+        loss = self._criteria(q_value, q_target)
+
         # optimize
         self._optimizer.zero_grad()
         loss.backward()
@@ -99,9 +148,10 @@ class DQN:
         self._optimizer.step()
 
     def _update_target_network(self):
-        '''update target network by copying from behavior network'''
-        ## TODO ##
-        raise NotImplementedError
+        '''
+        update target network by copying from behavior network
+        '''
+        self._target_net.load_state_dict(self._behavior_net.state_dict())
 
     def save(self, model_path, checkpoint=False):
         if checkpoint:
@@ -122,16 +172,31 @@ class DQN:
         if checkpoint:
             self._target_net.load_state_dict(model['target_net'])
             self._optimizer.load_state_dict(model['optimizer'])
+    
+    @staticmethod
+    def reshape_input_state(state):
+        state_shape = len(state.shape)
+        if state_shape == 1:
+            state = state.unsqueeze(0)
+        elif state_shape != 2:
+            raise ValueError(f'Wrong state shape: {state_shape}')
 
+        return state
+
+# -------------------------------------------------------
 
 def train(args, env_name, agent, writer):
-    print('Start Training')
-    env = gym.make(env_name)
+    logging.info('* Start Training')
+
+    env          = gym.make(env_name)
     action_space = env.action_space
+
     total_steps, epsilon, ewma_reward = 0, 1., 0.
+
     for episode in range(args.episode):
         total_reward = 0
-        state = env.reset()
+        state        = env.reset()
+
         for t in itertools.count(start=1):
             # select action
             if total_steps < args.warmup:
@@ -146,17 +211,18 @@ def train(args, env_name, agent, writer):
             if total_steps >= args.warmup:
                 agent.update(total_steps)
 
-            state = next_state
+            state         = next_state
             total_reward += reward
-            total_steps += 1
+            total_steps  += 1
+
             if done:
                 ewma_reward = 0.05 * total_reward + (1 - 0.05) * ewma_reward
                 writer.add_scalar('Train/Episode Reward', total_reward,
                                   total_steps)
                 writer.add_scalar('Train/Ewma Reward', ewma_reward,
                                   total_steps)
-                print(
-                    'Step: {}\tEpisode: {}\tLength: {:3d}\tTotal reward: {:.2f}\tEwma reward: {:.2f}\tEpsilon: {:.3f}'
+                logging.info(
+                    '  - Step: {}\tEpisode: {}\tLength: {:3d}\tTotal reward: {:.2f}\tEwma reward: {:.2f}\tEpsilon: {:.3f}'
                     .format(total_steps, episode, t, total_reward, ewma_reward,
                             epsilon))
                 break
@@ -164,23 +230,33 @@ def train(args, env_name, agent, writer):
 
 
 def test(args, env_name, agent, writer):
-    print('Start Testing')
+    logging.info('* Start Testing')
     env = gym.make(env_name)
+
     action_space = env.action_space
-    epsilon = args.test_epsilon
-    seeds = (args.seed + i for i in range(10))
-    rewards = []
+    epsilon      = args.test_epsilon
+    seeds        = (args.seed + i for i in range(10))
+    rewards      = []
+
     for n_episode, seed in enumerate(seeds):
         total_reward = 0
         env.seed(seed)
         state = env.reset()
-        ## TODO ##
-        # ...
-        #     if done:
-        #         writer.add_scalar('Test/Episode Reward', total_reward, n_episode)
-        #         ...
-        raise NotImplementedError
-    print('Average Reward', np.mean(rewards))
+
+        for t in itertools.count(start=1):
+            #action = agent.select_action(state, epsilon, action_space)
+            action = agent.select_best_action(state)
+            # execute action
+            next_state, reward, done, _ = env.step(action)
+
+            state         = next_state
+            total_reward += reward
+            total_steps  += 1
+
+            if done:
+                writer.add_scalar('Test/Episode Reward', total_reward, n_episode)
+                break
+    logging.info('  - Average Reward', np.mean(rewards))
     env.close()
 
 
